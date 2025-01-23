b657e323146cc351d7b75c739e3ae175266a2927
James Crittenden <james.crittenden.main@gmail.com>
2025-01-23 15:55:54 -0500

    Added support for vs_code snippets in LuaSnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,18 +18,8 @@ return { -- Autocompletion
       -- end)(),
       config = function()
         require('luasnip.loaders.from_snipmate').lazy_load()
+        require('luasnip.loaders.from_vscode').lazy_load { paths = { './snippets/vscode/go', './snippets/vscode/bash' } }
       end,
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
     },
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
